a387b97837628ae51cd3fdfc0791ca907ba0d748
00003688 <kadiraydogdu@tcdd.gov.tr>
2023-01-30 00:18:37 +0300

    Dto lar oluşturuldu

--- NLayer.Core/Models/ProductFeature.cs
+++ NLayer.Core/Models/ProductFeature.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Principal;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NLayer.Core
+﻿namespace NLayer.Core.Models
 {
     public class ProductFeature
     {
@@ -13,8 +6,7 @@ namespace NLayer.Core
         public string Color { get; set; }
         public int Height { get; set; }
         public int Width { get; set; }
-
         public int ProductId { get; set; }
-        public Product Product { get; set; }
+        public virtual Product Product { get; set; }
     }
 }
---
+++ Nlayer.Service/Mapping/MapProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using NLayer.Core;
+using NLayer.Core.DTOs;
+using NLayer.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nlayer.Service.Mapping
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<Product,ProductDto>().ReverseMap();
+            CreateMap<Category,CategoryDto>().ReverseMap();   
+            CreateMap<ProductFeature,ProductFeatureDto>().ReverseMap();
+            CreateMap<ProductUpdateDto, Product>();
+        }
+    }
+}
